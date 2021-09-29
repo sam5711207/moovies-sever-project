6911a3c8d42296f7035b04a5b0ce3c0a2822afc8
sam5711207 <sam5711207ruth>
2021-09-29 14:17:52 +0300

    first

--- controllers/actors.js
+++ controllers/actors.js
@@ -1,11 +1,12 @@
 const request = require("request-promise")
 const poeple = require("../actors_list")  //מערך של שמות שחקנים 
 
-//id לפי המערך השמות של השחקנים לקבל מערך של 
+//  לפי המערך השמות של השחקנים לקבל מערך של
+// הסרטים ששחקו בהם 
 mooviesArray = async (req, res) => {
     let i = 0
     let moviesIdArray = poeple.map(async actorName => {
-        let page = i ++
+        let page = i++
         const uri = `https://api.themoviedb.org/3/search/person?api_key=7b1152a1222f2893d32b8d84ae5d9abc&search_type=ngram&query=${actorName}`;
         const options = {
             method: "GET",
@@ -18,16 +19,18 @@ mooviesArray = async (req, res) => {
         try {
             let result = await request(options);
             result = await JSON.parse(result)
-            let movie_id = result.results[0].known_for.map(movie => {
-                if(movie.media_type === "movie"){
-                return movie.id
-             } 
-            if(result.total_pages > 1) {
-                
-            }})
-            return await movie_id
+            // let movie_id = result.results.map(async res => {
+            //     if(res.known_for_department === "Acting"){
+                   let movie_id = result.results.known_for.map(media => {
+                    if (media.media_type === "movie"){
+                        return media.id
+                    }
+                })
+                    return await movie_id
+            // }})
+            // return Promise.all(movie_id)
         } catch (err) {
-            console.error(err);
+            // console.eror(err);
         }
     })
     return Promise.all(moviesIdArray) //על כל הרשימה  map בשביל לחכות לסיים את ה 
@@ -65,43 +68,43 @@ mooviesArray = async (req, res) => {
 //     })
 //     let moviesRes = await Promise.all(moviesArray)
 //     res.send(moviesRes)
-    // return Promise.all(moviesArray) //על כל הרשימה  map בשביל לחכות לסיים את ה 
+// return Promise.all(moviesArray) //על כל הרשימה  map בשביל לחכות לסיים את ה 
 // }
 
 mervalMovies = async (req, res) => {
     let movArray = await mooviesArray();
     console.log(movArray, "popo")
-    let i = 0;
-    // let y = 0
-let mervalsArray = movArray.map(async movie_id => {
-       let actorName = poeple[i ++]
-    //    let oneMoveArray = movArray[y ++]
-        const uri = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=7b1152a1222f2893d32b8d84ae5d9abc&language=en-US&append_to_response=credits`;
-        const options = {
-            method: "GET",
-            url: uri,
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            "rejectUnauthorized": false
-        };
-        try {
-            let result = await request(options);
-            result = await JSON.parse(result)
-            if( result.homepage.includes("https://www.marvel.com/movies") ){
-            console.log("kkk", result.homepage)
-            return await [{ Name: actorName, Moovies: result.title }]
-        }
-        else{
-            return;
-        }
-        } catch (err) {
-            console.error(err);
-        }
-    })
-    // return Promise.all(mervalsArray); //על כל הרשימה  map בשביל לחכות לסיים את ה 
-    let mervalsArrayHome = await Promise.all(mervalsArray)
-    res.send(mervalsArrayHome)
+    // let i = 0;
+    // // let y = 0
+    // let mervalsArray = movArray.map(async movie_id => {
+    //     let actorName = poeple[i++]
+    //     //    let oneMoveArray = movArray[y ++]
+    //     const uri = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=7b1152a1222f2893d32b8d84ae5d9abc&language=en-US&append_to_response=credits`;
+    //     const options = {
+    //         method: "GET",
+    //         url: uri,
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         "rejectUnauthorized": false
+    //     };
+    //     try {
+    //         let result = await request(options);
+    //         result = await JSON.parse(result)
+    //         if (result.homepage.includes("https://www.marvel.com/movies")) {
+    //             console.log("kkk", result.homepage)
+    //             return await [{ Name: actorName, Moovies: result.title }]
+    //         }
+    //         else {
+    //             return;
+    //         }
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+    // })
+    // // return Promise.all(mervalsArray); //על כל הרשימה  map בשביל לחכות לסיים את ה 
+    // let mervalsArrayHome = await Promise.all(mervalsArray)
+    // res.send(mervalsArrayHome)
 }
 
 
--- routes/actors_routes.js
+++ routes/actors_routes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 const actors_controllers = require('../controllers/actors')
 
-// router.get('/actorsMovies', actors_controllers.mooviesArray); // עוד אופציה  רשימת סרטים-שאלה ראשונה
+router.get('/actorsMovies', actors_controllers.mooviesArray); // עוד אופציה  רשימת סרטים-שאלה ראשונה
 // router.get('/actorsMovies', actors_controllers.actorsMovies); //רשימת סרטים-שאלה ראשונה
-router.get('/actorsMovies', actors_controllers.mervalMovies); // רשימת סרטי מרוול-שאלה ראשונה
+// router.get('/actorsMovies', actors_controllers.mervalMovies); // רשימת סרטי מרוול-שאלה ראשונה
 // router.get('/actorsMovies', actors_controllers.bonus); //בונוס
 
 
